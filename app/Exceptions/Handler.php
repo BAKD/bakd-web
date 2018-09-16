6cc7974c5c9e008a5ae1f8b4b214703acd520d08
Tom Grenier <tom@bakd.io>
2018-09-15 22:35:53 -0400

    Framework setup continued, migrations started, configs published

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Exceptions;
+namespace BAKD\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace BAKD\Http\Controllers\Auth;
 
-use App\User;
-use App\Http\Controllers\Controller;
+use BAKD\User;
+use BAKD\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -59,7 +59,7 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\User
+     * @return \BAKD\User
      */
     protected function create(array $data)
     {
