2cf6f490e5012f536a5f7ec18717ed2c57055860
Tom Grenier <tom@bakd.io>
2018-09-21 23:42:37 -0400

    only show the annoying 'live development' modal on the homepage, not every page

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -39,4 +39,13 @@
             </div>
         </div>
     </section>
+
+    {{--
+        Show annoying warning modal (only on this page) about the current development disarray only in production -- and
+        only for the time being until we get to the point where we have something worthwhile to show.
+    --}}
+    @if (!app()->environment('local')))
+        @include('partials/modals/warning-modal')
+    @endif;
+
 @endsection
--- resources/views/partials/modals/all.blade.php
+++ resources/views/partials/modals/all.blade.php
@@ -1,10 +1,2 @@
 {{-- TODO: Wire this up --}}
 @include('partials/modals/contact-us')
-
-{{--
-    Show annoying warning modal about the current development disarray only in production -- and
-    only for the time being until we get to the point where we have something worthwhile to show.
---}}
-@if (app()->environment('production')))
-    @include('partials/modals/warning-modal')
-@endif;
