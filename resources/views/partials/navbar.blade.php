b583f72786619d1597995a2598b8858675928ddd
Tom Grenier <tom@bakd.io>
2018-09-22 00:47:46 -0400

    Rough responsive navbar fixes, misc minor style tweaks, auth'd login menu updates

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -5,9 +5,6 @@
                 <a class="navbar-brand" href="{{ route('frontend.home') }}">
                     <img class="bakd-logo" src="{{ config('bakd.logo.regular') }}" />
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
             </div>
             <div class="forum-bar user-types-nav">
                 <ul>
