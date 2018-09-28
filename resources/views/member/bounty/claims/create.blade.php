04985fce64675c428c9c9137162ce68845e4a504
Tom Grenier <tom@bakd.io>
2018-09-28 13:36:05 -0400

    Temp cleanup

--- resources/views/member/bounty/claims/create.blade.php
+++ resources/views/member/bounty/claims/create.blade.php
@@ -6,17 +6,10 @@
         <div class="container">
 
             <div class="widget widget-user">
-                <h3 class="title-wd"><i class="fa fa-star"></i>
-                    Claim Bounty {{ $bounty->id }}
+                <h3 class="title-wd"><i class="fa fa-btc"></i>
+                    Claim Bounty #{{ $bounty->id }} ({{ $bounty->name }})
                 </h3>
 
-                <label>
-                    Required Claim Information:
-                </label>
-
-                <textarea placeholder="Enter the bounty claim information for this bounty.">
-
-                </textarea>
 
             </div>
         </div>
