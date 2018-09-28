defa69d162f74751a1aa38b1a0e5d30d9d9892cb
Tom Grenier <tom@bakd.io>
2018-09-27 22:34:27 -0400

    Quick responsive fix

--- resources/views/member/bounty/index.blade.php
+++ resources/views/member/bounty/index.blade.php
@@ -4,21 +4,66 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">Member's Bounties</div>
-
-                <div class="card-body text-center">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <div class="under-construction">
-                        <i class="fa fa-exclamation-triangle fa-red fa-2x"></i>
-                        <h2 style="position: relative; top: -2px;">Currently In Development</h2>
+            <div class="post-bar">
+                <div class="epi-sec" style="padding: 20px 20px 0 20px; border-bottom: 1px solid #eee;">
+                    <ul class="descp">
+                        <li>
+                            <i class="la la-star"></i> <strong>Bounty Dashboard</strong>
+                        </li>
+                    </ul>
+                    <ul class="bk-links" style="padding-bottom: 15px; padding-right: 32px;" class="pull-right">
+                        <li>
+                            Test 123
+                        </li>
+                    </ul>
+                    <div class="ed-opts pull-right" style="float: right; position: absolute; right: 20px; top: 15px;">
+                        <a href="#" title="View Options" class="ed-opts-open"><i class="la la-ellipsis-v"></i></a>
+                        <ul class="ed-options" style="min-width: 160px;">
+                            <li>
+                                <i class="fa fa-facebook"></i> <a href="#" title="Share Bounty">Share Bounty</a>
+                            </li>
+                            <li>
+                                <i class="fa fa-plus"></i> <a href="#" title="Claim Bounty">Claim Bounty</a>
+                            </li>
+                            <li style="padding: 0; margin: -10px 0 0 0;"><hr></li>
+                            <li class="disabled">
+                                <i class="fa fa-bookmark"></i> <a href="#" class="disabled" title="Coming Soon">Bookmark</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
+
+
+                <div class="post_topbar">
+                    <div class="usy-dst row">
+                        <div class="col-lg-3">
+                        </div>
+                        <div class="col-lg-9">
+
+
+
+
+                            <div class="bounty-details">
+                                <h2 class="title">
+                                    Description 123
+                                </h2>
+                                <div>
+                                    Test 123
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="job-status-bar text-right" style="border-top: 1px solid #eee;">
+                    <div class="claim-bounty-button text-right">
+                        <a href="#" type="button" class="btn btn-primary btn-large">
+                            <i class="fa fa-star"></i> CLAIM BOUNTY
+                        </a>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
