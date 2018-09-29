77f114e24c9e57b6b82d76f55609e5a9698ff3e8
Tom Grenier <tom@bakd.io>
2018-09-29 00:20:54 -0400

    Add common error pages similar to BAKD layout for our server-error-screen-shot-takin' 'supporters'

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.frontend')
+
+@section('content')
+    <div class="container">
+        <div class="widget widget-user">
+            <h3 class="title-wd"><i class="fa fa-red fa-times-circle"></i>
+                Service Unavailable | 503
+            </h3>
+            <table class="unselectable bounty-announcements-table table-responsive table centered-td">
+                <tbody>
+                    <tr>
+                        <td class="text-center" style="padding: 20px;">
+                            <i class="fa fa-red fa-2x fa-exclamation-triangle"></i>
+                            <div>
+                                <strong>Service Unavailable</strong>
+                                <div style="font-style: italic; padding: 10px;">(Our developers have already been notified... this is an alpha afterall!)</div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
