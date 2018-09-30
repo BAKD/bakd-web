c9663d95af2d4697b3f63ada19a24b70893e618f
Tom Grenier <tom@bakd.io>
2018-09-30 12:22:15 -0400

    Only show claim info alert when necessary

--- resources/views/member/bounty/_info-alert.blade.php
+++ resources/views/member/bounty/_info-alert.blade.php
@@ -1,17 +1,19 @@
-<div class="text-left alert alert-warning alert-dismissable" role="alert">
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-    </button>
-    <div class="alert-message">
-        <i class="alert-icon fa fa-info-circle"></i>
-        <span>
-            @if ($bounty->isOver())
-                Bounty has already ended!
-            @elseif ($bounty->isPaused())
-                Bounty is currently paused!
-            @elseif (!$bounty->isStarted())
-                Bounty has not started yet!
-            @endif
-        </span>
+@if ($bounty->isOver() || $bounty->isPaused() || !$bounty->isStarted())
+    <div class="text-left alert alert-warning alert-dismissable" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+        <div class="alert-message">
+            <i class="alert-icon fa fa-info-circle"></i>
+            <span>
+                @if ($bounty->isOver())
+                    Bounty has already ended!
+                @elseif ($bounty->isPaused())
+                    Bounty is currently paused!
+                @elseif (!$bounty->isStarted())
+                    Bounty has not started yet!
+                @endif
+            </span>
+        </div>
     </div>
-</div>
+@endif
