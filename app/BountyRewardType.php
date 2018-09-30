80310279e6b72c766d16fb2439e9357119f034ed
Tom Grenier <tom@bakd.io>
2018-09-29 20:23:56 -0400

    Admin tweaks, sortable columns, reward type relationship change

--- app/BountyRewardType.php
+++ app/BountyRewardType.php
@@ -44,6 +44,6 @@ class BountyRewardType extends Model
 
     public function bounty()
     {
-        return $this->belongsTo('BAKD\Bounty');
+        return $this->hasMany('BAKD\Bounty');
     }
 }
