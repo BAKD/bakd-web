6421d5e15b70db2cd5d1505e729d7f87a0e0f06f
Tom G <tom@bakd.io>
2018-10-07 18:45:07 -0400

    Remove junk. Add basic queries to member and (future) campaign public facing page stubs.

---
+++ app/Campaign.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace BAKD;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Campaign extends Model
+{
+    use Traits\Uuids,
+        SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'campaigns';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+}
