a8c3b642e1ed6fe18a69fd43ea64912a5f2ff737
Tom Grenier <tom@bakd.io>
2018-10-21 01:59:27 -0400

    Advert image fix

--- resources/views/components/advert.blade.php
+++ resources/views/components/advert.blade.php
@@ -1,5 +1,5 @@
 <div class="widget widget-adver">
     <a href="https://bakd.me/member/bounty/18" alt="Spread the Word Bounty is Live!" title="Spread the Word Bounty is Live!">
-        <img src="{{ asset('/images/ads/spread-word-banner.jpg') }}" alt="Spread the Word Bounty is Live!" />
+        <img src="{{ asset('/images/ads/spread-word-banner.jpg') }}" alt="Spread the Word Bounty is Live!" style="max-width: 690px; width: 100%; height: auto;" />
     </a>
 </div>
